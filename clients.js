18775a462f90db03a49598a36f5749ab4fa90e8a
Holly Ann Zurenda <zurend55@students.rowan.edu>
2021-06-18 11:21:57 -0400

    Initial project push and functionality testing

---
+++ clients.js
@@ -0,0 +1,37 @@
+const clients = [
+    {
+        id: 1,
+        firstName: 'Jordan',
+        lastName: 'Jiosi',
+        address: '1650 Market St.',
+        city: 'Philadelphia',
+        state: 'PA'
+    }, 
+    {
+        id: 2,
+        firstName: 'Hermione',
+        lastName: 'Granger',
+        address: '7 Brighton Pl.',
+        city: 'Ocean City',
+        state: 'NJ'
+    }, 
+    {
+        id: 3,
+        firstName: 'Bryce',
+        lastName: 'Quinlin',
+        address: '78 Brookfield St.',
+        city: 'Seakonk',
+        state: 'MA'
+    }, 
+    {
+        id: 4,
+        firstName: 'Hunt',
+        lastName: 'Athalar',
+        address: '10 Tray Hollow Rd.',
+        city: 'Foster',
+        state: 'RI'
+    }, 
+]
+
+
+export default clients;
---
+++ index.js
@@ -0,0 +1,55 @@
+import Express from 'express';
+import Clients from "./clients.js";
+
+
+const app = Express();
+const port = 4000;
+app.use(Express.json())
+app.use(Express.urlencoded({ extended: true}))
+
+app.get("/", (req, res) => {
+    // req.query
+    // req.params
+    res.send("<h1>Welcome!</h1>")
+    })
+
+function customCallback(req, res, next){
+    console.log(req.body);
+    console.log(req.params);
+    //CALLS CALLBACK FOR ME
+    next();
+}
+
+app.get("/clients/", (req, res) => {
+    // req.query
+    // req.params
+
+    res.json(Clients)
+    })
+
+// app.get("/clients/:id", (req, res) => {
+//     // req.query
+//     // req.params
+//     res.json(Clients.find((client) => {
+//         return +req.params.id === client.id
+//     }))
+//      
+//     })
+
+app.get("/clients/:id", (req, res) => {
+    // req.query
+    // req.params
+    res.json(Clients.find((client) => {
+        return +req.params.id === client.id
+    }))
+     
+    })
+
+app.post("/add", (req, res) => {
+    console.log(req.body.id)
+    // res.send(req.body.id)
+    res.sendStatus(200);
+
+})
+
+app.listen(port, ()=> console.log("Listening on port: " + port));
