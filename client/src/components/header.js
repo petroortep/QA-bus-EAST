326c1ac16ccef810f448d1faba16b4cfabb055ed
Holly Ann Zurenda <zurend55@students.rowan.edu>
2021-06-19 12:45:00 -0400

    finished homepage style (needs working menu)

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../logo.png';
+import logo2 from '../surfLogo.png';
 import './header.css';
 import './menubar';
 import MenuBar from './menubar';
@@ -11,8 +12,10 @@ class Header extends Component {
         <div className="blk-background">
         <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className='App-title'>Jeremy Roofing Co.</h1>
+          <img style={{marginBottom: "30pt"}} src={logo} className="App-logo" alt="logo"/>
+          <h1 style={{marginTop: "-40pt", marginBottom: "-40pt", paddingBottom: "5pt", float: "left" }} className='App-title'>Jeremy Lyson</h1>
+          <h6 className='App-Subtitle'> of</h6>
+          <img style={{paddingBottom: "20pt", marginBottom: "-40pt", marginTop: "-70pt", paddingTop: "5pt"}}src={logo2} className="App-logo2" alt="logo2" width="150px" height="auto" />
           <MenuBar/>
         </header>
         </div>
